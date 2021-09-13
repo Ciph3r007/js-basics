a28abf647ae2c4a46ccce07e36ee7dd85865c0b4
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-13 13:32:58 +0600

    Update oop_exercises.js

--- oop_exercises.js
+++ oop_exercises.js
@@ -40,3 +40,46 @@ function Stopwatch() {
 }
 
 const sw = new Stopwatch();
+
+
+/* Two constructors for HtmlElement and HtmlSelectElement
+ * HtmlSelectElement inherits HtmlElement
+
+ * HtmlElement has:
+ * - click(), an instance method
+ * - focus(), a prototype method
+ * 
+ * HtmlSelectElement takes optional `items` array as input and has:
+ * - addItem() and removeItem(), both are instance methods
+ */
+
+function HtmlElement() {
+    this.select = function () {
+        console.log('selected');
+    }
+}
+
+HtmlElement.prototype.focus = function () {
+    console.log('focused');
+}
+
+const html = new HtmlElement()
+
+function HtmlSelectElement(items=[]) {
+    this.items = items
+
+    this.addItem = item => this.items.push(item);
+
+    this.removeItem = function (item) {
+        const index = items.indexOf(item);
+        items.splice(index, 1);
+    }
+}
+
+// DOING THIS INSTEAD WOULD NOT INHERIT INSTANCE METHODS
+// HtmlSelectElement.prototype = Object.create(HtmlElement.prototype);
+
+HtmlSelectElement.prototype = new HtmlElement();
+HtmlSelectElement.prototype.constructor = HtmlSelectElement; // not necessary, just best practice
+
+const htmlSelect = new HtmlSelectElement([1, 2, 3]);
